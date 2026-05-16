625febc3461952d2755041f0661a16aa59cb3889
JustDrven <petr.nemec11111@gmail.com>
2026-05-16 11:33:43 +0200

    feat: add connection pool management

--- src/application/orm/database.rs
+++ src/application/orm/database.rs
@@ -1,18 +1,25 @@
 use std::collections::HashMap;
-use std::ptr::null;
-use orma::Connection;
+use std::sync::{Arc, OnceLock};
+
 use crate::application::enums::logger::LoggerType;
 use crate::application::util::logger;
 
-pub fn initialize(variables: &mut HashMap<String, String>) -> Result<bool, String> {
+static DB_CLIENT: OnceLock<Arc<Client>> = OnceLock::new();
+
+pub async fn initialize(variables: &mut HashMap<String, String>) -> Result<bool, String> {
 
     let host_key = "DB_HOST";
+    let port_key = "DB_PORT";
     let username_key = "DB_USERNAME";
     let password_key = "DB_PASSWORD";
     let db_key = "DB_NAME";
 
     if !variables.contains_key(host_key) {
         return Err("Missing host variable".to_string());
+    }
+
+    if !variables.contains_key(port_key) {
+        return Err("Missing port variable".to_string());
     }
 
     if !variables.contains_key(username_key) {
@@ -27,22 +34,52 @@ pub fn initialize(variables: &mut HashMap<String, String>) -> Result<bool, Strin
         return Err("Missing database name variable".to_string());
     }
 
-    let host = variables.get(host_key).unwrap();
-    let username = variables.get(username_key).unwrap();
-    let password = variables.get(password_key).unwrap();
-    let db_name = variables.get(db_key).unwrap();
+    let host: &String = variables.get(host_key).unwrap();
+    let port: &String = variables.get(port_key).unwrap();
+    let username: &String = variables.get(username_key).unwrap();
+    let password: &String = variables.get(password_key).unwrap();
+    let db_name: &String = variables.get(db_key).unwrap();
 
-    logger::log(LoggerType::Info, format!("Opening database connection for {} - password: {}", host, !password.is_empty()));
-    let connection = create_connection(
-        host, username, password, db_name
-    );
+    {
+        logger::log(LoggerType::Info, format!("Opening database connection for {} - password: {}", host, !password.is_empty()));
+        let new_pool = create_connection(host, port, db_name, username, password).await?;
+
+        if DB_CLIENT.set(Arc::new(new_pool)).is_err() {
+            return Err("Database already initialized".to_string());
+        }
+    }
+
 
     Ok(true)
 }
 
-fn create_connection(host: &String, username: &String, password: &String, db_name: &String) -> bool {
+pub fn get_pool() -> Option<Arc<Client>> {
+    DB_CLIENT.get().cloned()
+}
 
-    todo!("Add connection to database");
-    true
+use tokio_postgres::{Client, NoTls};
 
+async fn create_connection(
+    host: &str,
+    port: &str,
+    db_name: &str,
+    username: &str,
+    password: &str) -> Result<Client, String> {
+    let connection_string = format!(
+        "host={} port={} dbname={} user={} password={}",
+        host, port, db_name, username, password
+    );
+
+
+    let (client, connection) = tokio_postgres::connect(&connection_string, NoTls)
+        .await
+        .map_err(|e| format!("Connection error: {}", e))?;
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Database connection error: {}", e);
+        }
+    });
+
+    Ok(client)
 }
