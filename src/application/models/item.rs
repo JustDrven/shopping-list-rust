da050546477bc11ccc205b9f95dc1e30f82d714e
JustDrven <petr.nemec11111@gmail.com>
2026-05-17 13:43:45 +0200

    refactor: fix name of model and add complete row

--- src/application/models/item.rs
+++ src/application/models/item.rs
@@ -1,12 +1,12 @@
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
-#[sea_orm(table_name = "users")]
+#[sea_orm(table_name = "items")]
 pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
     pub name: String,
-    
+    pub complete: bool,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
